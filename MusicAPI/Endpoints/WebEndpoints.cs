7fa89550699696b63df401257e62d78cad9fafaa
Cody Howell <codyhowell44@gmail.com>
2026-02-24 11:08:24 -0700

    progress on node uploads

--- MusicAPI/Endpoints/WebEndpoints.cs
+++ MusicAPI/Endpoints/WebEndpoints.cs
@@ -7,6 +7,7 @@ public static class WebEndpoints {
     public static WebApplication MapWebEndpoints(this WebApplication app, string start) {
         app.MapGet(start + "/web", (DBService service) => service.GetWebs());
         app.MapGet(start + "/web/{id}", (DBService service, int id) => service.GetWeb(id));
+        app.MapGet(start + "/web/{id}/nodes", (DBService service, int id) => service.GetNodesAsync(id));
 
         app.MapPost(start + "/web", (DBService service, WebInformation info) => service.CreateWeb(info));
 
